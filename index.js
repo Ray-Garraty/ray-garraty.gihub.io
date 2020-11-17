ad18b02afbc1c6d33eae9c9f6ef4606cecc37ad1
Ray-Garraty <vl.besed@gmail.com>
2020-11-17 17:14:41 +0300

    added a html generator

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var osmosis = require('osmosis');
+const fs = require('fs');
+const osmosis = require('osmosis');
+const htmlCreator = require('html-creator');
+
 const result = [];
 for (let j = 1; j <= 3; j += 1) {
     const url = j === 1 ? 'https://fuelcellsworks.com/news/' : `https://fuelcellsworks.com/news/page/${j}/`;
@@ -6,9 +9,9 @@ for (let j = 1; j <= 3; j += 1) {
         .get(url)
         .find('.article-content-wrap')
         .set({
-          Title: 'h2',
-          Date: '.desktop-reading',
-          Link: 'h2 > a@href'
+          title: 'h2',
+          date: '.desktop-reading',
+          link: 'h2 > a@href'
         })
         .data(function(content) {
           result.push({
@@ -20,8 +23,46 @@ for (let j = 1; j <= 3; j += 1) {
         .error(console.log)
         .debug(console.log)   
 };
-const showResults = (array) => {
+
+const createContentArray = (dataset) => dataset.map((element) => {
+    return [
+      {
+        type: 'p',
+        content: [
+          {
+            type: 'div',
+            content: element.pageContent.date,
+            attributes: {},
+          },
+          {
+            type: 'a',
+            content: element.pageContent.title,
+            attributes: { href: element.pageContent.link, target: '_blank' },
+          },
+        ],
+      },
+    ];
+  });
+  
+  const generateHtmlFromDataset = (contentArray) => {
+    const html = new htmlCreator([
+      {
+        type: 'head',
+        content: [{ type: 'title', content: 'Generated HTML' }]
+      },
+      {
+        type: 'body',
+        attributes: { style: 'padding: 1rem' },
+        content: contentArray,
+      },
+    ]);
+    return html;
+  };
+
+const sortAndWriteResultsToFile = (array) => {
   const sortedResult = array.sort((a, b) => a.pageNumber - b.pageNumber);
-  console.log(sortedResult);
+  const html = generateHtmlFromDataset(array);
+  fs.writeFileSync('index.html', html, 'utf-8');
 };
-setTimeout(showResults, 5000, result);
+
+setTimeout(sortAndWriteResultsToFile, 5000, result);
