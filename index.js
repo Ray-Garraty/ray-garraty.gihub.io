ed3eca85b1f99f2c345a343ba230cd509e4f3680
Ray-Garraty <vl.besed@gmail.com>
2020-11-18 17:12:20 +0300

    corrected the osmosis promises and newsArray construction logic

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const htmlCreator = require('html-creator');
 const firstPageUrlAddress = 'https://fuelcellsworks.com/news';
 const numberOfPages = 1;
 
-/* асинхронная функция scrapeTitle на основе osmosis: принимает на вход url-адрес и номер страницы, 
-возвращает промис, который разрешается в объект с данными одной новости */
-const scrapeTitle = async (url, num) => {
-  const result = [];
-  await osmosis
+/* обычная функция scrapeTitle на основе osmosis: принимает на вход url-адрес и номер страницы, 
+возвращает промис, который разрешается в объект с данными новостей этой страницы */
+const scrapeTitle = (url, num) => {
+  return new Promise((resolve, reject) => {
+    const result = [];
+    osmosis
     .get(url)
     .find('.article-content-wrap')
     .set({
@@ -23,31 +24,32 @@ const scrapeTitle = async (url, num) => {
         pageContent: content,
       });
     })
+    .done(() => resolve(result))
     .log(console.log)
     .error(console.log)
     .debug(console.log);
-  return result;
+  }); 
 };
 
 /* функция-генератор массива новостей generateNewsArray: принимает на вход url-адрес первой страницы и
 количество страниц, которое нужно обойти, формирует массив промисов, 
 дожидается разрешения Promise.all по ним, выдаёт массив новостей и сортирует его по номеру страницы */
 const generateNewsArray = async (firstPageUrl, numberOfPages) => {
-  const promises = [];
+  let promises = [];
   for (let i = 1; i <= numberOfPages; i += 1) {
     const url = `${firstPageUrl}/page/${i}/`;
-    const newsFromPage = scrapeTitle(url, i);
-    promises.concat(newsFromPage);
+    const newsFromPageI = scrapeTitle(url, i);
+    promises = [...promises, newsFromPageI];
   }
   const news = await Promise.all(promises);
   const sortedNews = news.sort((a, b) => a.pageNumber - b.pageNumber);
-  // console.log(sortedNews);
+  console.log(sortedNews);
   return sortedNews;
 };
 
 // простая функция createHtml, которая принимает на вход массив новостей и генерирует HTML-код с ними
 const createHtml = (news) => {
-  const contentArray = news.map((element) => {
+  const contentArray = news[0].map((element) => {
     const result = {
       type: 'p',
       content: [
@@ -90,68 +92,3 @@ generateNewsArray(firstPageUrlAddress, numberOfPages)
 .then((newsArray) => createHtml(newsArray))
 .then((html) => writeHtmlToFile(html))
 .catch(console.error);
-
-/* const result = [];
-for (let j = 1; j <= 3; j += 1) {
-    const url = j === 1 ? 'https://fuelcellsworks.com/news/' : `https://fuelcellsworks.com/news/page/${j}/`;
-    osmosis
-        .get(url)
-        .find('.article-content-wrap')
-        .set({
-          title: 'h2',
-          date: '.desktop-reading',
-          link: 'h2 > a@href'
-        })
-        .data(function(content) {
-          result.push({
-            pageNumber: j,
-            pageContent: content,  
-          })
-        })
-        .log(console.log)
-        .error(console.log)
-        .debug(console.log)   
-};
-
-const createContentArray = (dataset) => dataset.map((element) => {
-    return [
-      {
-        type: 'p',
-        content: [
-          {
-            type: 'div',
-            content: element.pageContent.date,
-            attributes: {},
-          },
-          {
-            type: 'a',
-            content: element.pageContent.title,
-            attributes: { href: element.pageContent.link, target: '_blank' },
-          },
-        ],
-      },
-    ];
-  });
-  
-  const generateHtmlFromDataset = (contentArray) => {
-    const html = new htmlCreator([
-      {
-        type: 'head',
-        content: [{ type: 'title', content: 'Generated HTML' }]
-      },
-      {
-        type: 'body',
-        attributes: { style: 'padding: 1rem' },
-        content: contentArray,
-      },
-    ]);
-    return html;
-  };
-
-const sortAndWriteResultsToFile = (array) => {
-  const sortedResult = array.sort((a, b) => a.pageNumber - b.pageNumber);
-  const html = generateHtmlFromDataset(array);
-  fs.writeFileSync('index.html', html, 'utf-8');
-};
-
-setTimeout(sortAndWriteResultsToFile, 5000, result); */
