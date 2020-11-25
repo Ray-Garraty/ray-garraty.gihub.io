b438278d74ee603428f0f6b123e8a5931d601b55
Ray-Garraty <vl.besed@gmail.com>
2020-11-25 17:04:39 +0300

    some more minor changes

--- lib.js
+++ lib.js
@@ -17,12 +17,14 @@ const scrapeNewsFromHSE = () => {
         link: 'a@href',
       })
       .data(function(content) {
-        const array = content.date.split(' ');
-        const [m, y] = array;
-        console.log(array, 'Месяц: ', m, 'Год: ', y);
-        const month = m.slice(1);
-        const year = y.slice(0, -1);
-        content.date = `01 ${src.translateMonth(month)} ${year}`;
+        const regex = /\s|,\s/;
+        if (content.date) {
+          const array = content.date.split(regex);
+          const [m, y] = array;
+          const month = m.slice(1);
+          const year = y.slice(0, -1);
+          content.date = `01 ${src.translateMonth(month)} ${year}`;
+        }
         result.push(content);
       })
       .done(() => {
@@ -35,8 +37,66 @@ const scrapeNewsFromHSE = () => {
     }); 
 };
 
+const scrapeNewsFromSaft = () => {
+  return new Promise((resolve, reject) => {
+    const result = [];
+    osmosis
+      .get('https://www.saftbatteries.com/media-resources/press-releases')
+      .find('li.content-list-item')
+      .set({
+        title: 'h4',
+        date: 'span.date-display-single',
+        link: 'a@href',
+      })
+      .data(function(content) {
+        const domainName = 'https://www.saftbatteries.com';
+        content.link = content.link.includes(domainName) 
+        ? content.link 
+        : `${domainName}${content.link}`;
+        content.date = content.date.split('/').reverse().join('/');
+        result.push(content);
+      })
+      .done(() => resolve(result))
+      .log(console.log)
+      .error(console.log)
+      .debug(console.log);
+    }); 
+};
+
+const scrapeNewsFromLiotech = () => {
+  return new Promise((resolve, reject) => {
+    const result = [];
+    osmosis
+      .get('https://www.liotech.ru/media/')
+      .find('div.news-block')
+      .set({
+        title: 'div.news-block__title',
+        date: 'div.news-block__date',
+        link: '@data-href',
+      })
+      .data(function(content) {
+        const domainName = 'https://www.liotech.ru';
+        content.link = content.link.includes(domainName) 
+        ? content.link 
+        : `${domainName}${content.link}`;
+        const dateArray = content.date.split(' ').slice(0, 2);
+        const day = dateArray[0];
+        const month = dateArray[1].substr(-4);
+        const year = dateArray[1].substr(-4);
+        content.date = `${day} ${src.translateMonth(month)} ${year}`;
+        result.push(content);
+      })
+      .done(() => resolve(result))
+      .log(console.log)
+      .error(console.log)
+      .debug(console.log);
+    }); 
+};
+
 const websitesList = [
   scrapeNewsFromHSE(),
+  scrapeNewsFromSaft(),
+  scrapeNewsFromLiotech(),
 ];
   
 src.generateNewsArray(websitesList)
--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const osmosis = require('osmosis');
 // osmosis.config('user_agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36');
 osmosis
-    .get('https://energy.hse.ru/accenergy')
-    .find('div.with-indent5._builder.builder--text')
+    .get('https://www.liotech.ru/media/')
+    .find('div.news-block')
     .set({
-      title: 'em',
-      date: 'p > span:nth-child(2)',
-      link: 'a@href',
+      title: 'div.news-block__title',
+      date: 'div.news-block__date',
+      link: '@data-href',
     })
     .data(function(content) {
       console.log(content);
