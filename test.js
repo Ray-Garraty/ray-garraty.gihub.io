5be55814060218beacb2c2299b87c6e87ab33cf1
Ray-Garraty <vl.besed@gmail.com>
2020-11-26 21:47:28 +0300

    started work on yandex scraper

--- test.js
+++ test.js
@@ -2,20 +2,32 @@
 
 const osmosis = require('osmosis');
 // osmosis.config('user_agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36');
+
+const queryString = '%D0%BB%D0%B8%D1%82%D0%B8%D0%B9-%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B5+%D0%B0%D0%BA%D0%BA%D1%83%D0%BC%D1%83%D0%BB%D1%8F%D1%82%D0%BE%D1%80%D1%8B';
+
+osmosis.config('user_agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36');
+osmosis.config('tries', 1)
+osmosis.config('concurrency', 2);
+
 osmosis
-    .get(`https://newssearch.yandex.ru/yandsearch?text=${inquiry}&rpt=nnews2&grhow=clutop&rel=tm`)
+    .get(`https://newssearch.yandex.ru/yandsearch?text=${queryString}&rpt=nnews2&grhow=clutop&wiz_no_news=1&rel=tm`)
+    .then(function(context, data, next) {
+      console.dir(context);
+    })
+    /* .paginate('div.pager__content :nth-child(3)')
+    .delay(2000)
     .find('div.documents :first-child')
     .set({
-      title: 'a',
+      title: 'div.document__title > a',
       date: 'div.document__time',
-      link: 'a@href',
-    })
+      link: 'div.document__title > a@href',
+    }) */
     .data(function(content) {
-      content.date = content.date.split('|')[1].toString().trim();
       console.log(content);
     })
     .log(console.log)
     .error(console.log)
     .debug(console.log);
+    
 
     
